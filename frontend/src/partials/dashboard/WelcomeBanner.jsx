970b6e3cda445723a321d9f764d2da8cfb03a020
rudradeep22 <121369407+rudradeep22@users.noreply.github.com>
2023-10-05 15:20:41 +0530

    fixed bugs due to connection

--- frontend/src/partials/dashboard/WelcomeBanner.jsx
+++ frontend/src/partials/dashboard/WelcomeBanner.jsx
@@ -21,10 +21,10 @@ function WelcomeBanner() {
     <div className="relative bg-indigo-200 dark:bg-indigo-500 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
       {/* Content */}
       <div className="flex justify-around">
-        <img src={Icon}alt="Icon" className='w-16 b-6' />
+        {/* <img src={Icon}alt="Icon" className='w-16 b-6' /> */}
         <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">{getGreeting()} 👋</h1>
         <p className="dark:text-indigo-200 text-2xl md:text-3xl">  Here is what’s happening with our projects today</p>
-        <img src={Icon}alt="Icon" className='w-16 b-6'/>
+        {/* <img src={Icon}alt="Icon" className='w-16 b-6'/> */}
       </div>
     </div>
   );
