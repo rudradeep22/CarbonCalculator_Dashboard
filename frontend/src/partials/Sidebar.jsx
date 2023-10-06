e279ee46abf3f978c2defec68195413d100025de
rudradeep22 <121369407+rudradeep22@users.noreply.github.com>
2023-10-07 02:22:44 +0530

    Improved ui

--- frontend/src/partials/Sidebar.jsx
+++ frontend/src/partials/Sidebar.jsx
@@ -56,8 +56,8 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
       {/* Sidebar */}
       <div
         id="sidebar"
-        ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-green-600 p-4 transition-all duration-200 ease-in-out dark:bg-indigo-700 ${
+        ref={sidebar}w-48
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar  lg:w-20 lg:sidebar-expanded:!w-48 2xl:!w-48 shrink-0 bg-green-600 p-4 transition-all duration-200 ease-in-out dark:bg-indigo-700 ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-64'
         }`}
       >
--- frontend/src/partials/dashboard/DashBoardNetZeroIITK.jsx
+++ frontend/src/partials/dashboard/DashBoardNetZeroIITK.jsx
@@ -11,7 +11,7 @@ function DashBoardNetZeroArmyCantt({netZeroIITKStatus}) {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-6 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <div className="px-5 pt-5">
-        <div className='flex flex-wrap justify-between items-center'>
+        <div className='flex flex-col flex-wrap justify-between items-center xl:flex-row'>
           <h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2 xl:text-2xl">Status of Net Zero IITK&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</h2>
           <div style={{ width: 100, height: 100 }}>
           <CircularProgressbar
