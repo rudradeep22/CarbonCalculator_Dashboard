eb38d859524c1b195d687f5e636119e4e5ae8dee
rudradeep22 <121369407+rudradeep22@users.noreply.github.com>
2023-10-21 23:14:40 +0530

    Fixed bugs in run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Define a function to handle termination
-cleanup() {
-    echo "Terminating background processes..."
-    killall node
+# Use below for linux systems
+# cleanup() {
+#     echo "Terminating background processes..."
+#     killall node
+#     exit
+# }
+
+# Use below for windows systems
+cleanup(){
+    echo "Terminating all processes"
+    cmd "/C TASKKILL /IM node.exe /F"
     exit
 }
 
 # Register the cleanup function to run when the script receives a SIGINT (Ctrl+C)
 trap cleanup SIGINT
 
-# Start the frontend development server
 (cd frontend && npm run dev) &
-
-# Save the PID of the frontend process
 frontend_pid=$!
 
-# Start the backend development server
 (cd backend && npm run dev) &
-
-# Save the PID of the backend process
 backend_pid=$!
 
-# Wait for user to press 'q'
 echo "Press 'q' to terminate background process..."
 
 while :; do
