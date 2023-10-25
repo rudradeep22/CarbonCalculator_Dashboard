fef64c68d4325ae452655f5af1d4ee92ac96253e
rudradeep22 <121369407+rudradeep22@users.noreply.github.com>
2023-10-25 22:52:54 +0530

    Enhanced dev run commands

--- run.sh
+++ run.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
-# Use below for linux systems
-# cleanup() {
-#     echo "Terminating background processes..."
-#     killall node
-#     exit
-# }
+if [[ "$OSTYPE" == "linux"* ]]; then
+    # Linux system
+    cleanup() {
+        echo "Terminating background processes..."
+        killall node
+        exit
+    }
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    # Windows system
+    cleanup() {
+        echo "Terminating all processes"
+        cmd "/C TASKKILL /IM node.exe /F"
+        exit
+    }
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
 
-# Use below for windows systems
-cleanup(){
-    echo "Terminating all processes"
-    cmd "/C TASKKILL /IM node.exe /F"
-    exit
-}
-
-# Register the cleanup function to run when the script receives a SIGINT (Ctrl+C)
 trap cleanup SIGINT
 
-(cd frontend && npm run dev) &
-frontend_pid=$!
+# Replace Mongodb uri with your own
+if [ ! -f "frontend/.env" ]; then
+    echo "VITE_URL=http://localhost:3000" > frontend/.env
+    echo "MONGODB_URI=<Your mongodb uri" >> frontend/.env
+    echo "VITE_PORT=3000" >> frontend/.env
+fi
 
+if [ ! -f "backend/.env" ]; then
+    echo "VITE_URL=http://localhost:5173/" > backend/.env
+    echo "MONGODB_URI=<Your Mongodb uri>" >> backend/.env
+    echo "VITE_PORT=3000" >> backend/.env
+    echo "TOKEN_KEY=<Anything string>" >> backend/.env
+fi
+
+(cd frontend && npm run dev) &
 (cd backend && npm run dev) &
-backend_pid=$!
 
 echo "Press 'q' to terminate background process..."
 
 while :; do
     read -n 1 key
     if [[ $key == "q" ]]; then
-        cleanup $frontend_pid $backend_pid
+        cleanup
     fi
 done
